0e81a8d28e6873ad4f601708561e965669f5e89e
juanseca94 <juanseca_94@hotmail.com>
2014-12-12 15:54:15 -0500

    mas clases...

---
+++ src/Client/Cliente.java
@@ -0,0 +1,44 @@
+package Client;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class Cliente {
+	String dirIP;
+	int numPuerto=9999;
+	Scanner sDato = new Scanner(System.in);
+	
+	public void conexiones()
+	{	String mensajeOut,mensajeIn;
+		System.out.println("Conectandose al Servidor.... Abriendo Puerto"+numPuerto);
+		try {
+			dirIP=JOptionPane.showInputDialog("Digite la IP del Servidor:");
+			Socket b = new Socket(dirIP,numPuerto);
+			System.out.println("Conectado!");
+			
+			DataOutputStream out = new DataOutputStream(b.getOutputStream());
+			do{
+				System.out.println("Digite el mensaje a enviar:");
+				mensajeOut = sDato.nextLine();
+				out.writeUTF(mensajeOut);
+				if(mensajeOut.equals("fin")) break;
+				System.out.println("Mensaje ["+mensajeOut+"] Enviado!");
+				System.out.println("Esperando Respuesta!...");
+				DataInputStream in = new DataInputStream(b.getInputStream());
+				mensajeIn = in.readUTF();
+				System.out.println(b.getInetAddress()+"Dice: "+mensajeIn);
+				
+			}while(!mensajeOut.equals("fin"));
+			 System.out.println("Cerro sesion");
+		} catch (Exception e) {	}
+		
+	}
+	
+	public static void main(String[] args) {
+		new Cliente().conexiones();
+
+	}
+
+}
